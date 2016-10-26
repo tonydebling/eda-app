cc69313a4c8f88028be6b9aece9de2cd3478efe7
Tony Debling <tonydebling@gmail.com>
2016-10-26 14:16:16 +0100

    Update home.php image reference
    Images moved into public directory, so image reference must be changed

--- views/home.php
+++ views/home.php
@@ -3,6 +3,6 @@
 {% block title %}HOME{% endblock %}
 {% block content %}
 	<div>
-	  <img src="../images/exam-hall.jpg" class="w3-border w3-padding" style="width:100%">
+	  <img src="./images/exam-hall.jpg" class="w3-border w3-padding" style="width:100%">
 	</div>
 {% endblock %}
