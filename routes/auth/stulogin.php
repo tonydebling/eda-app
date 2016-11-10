08feaa2fec3f538036742f84d7b9b22345a989d2
Tony Debling <tonydebling@gmail.com>
2016-11-10 15:59:42 +0000

    Fix staff login sequence
    School id is not entered as part of the staff registration process

--- routes/auth/stulogin.php
+++ routes/auth/stulogin.php
@@ -33,7 +33,7 @@ $app->post('/stufindschool', $guest(), function() use($app) {
 	} else {
 		$app->render('auth/stufindschool.php');
 	};
-
+	
 	die();
 	
 })->name('stufindschool.post');
@@ -57,28 +57,29 @@ $app->post('/stulogin', $guest(), function() use($app) {
 	]);
 	
 	if ($v->passes()){
+		/* Need to include DOB check */
 		$student = $app->student
 			->where('first_name', $first_name)
 			->where('last_name', $last_name)
 			->where('school_id', $school_id)
 			->first();
-			
-		/* CHECK STUDENT FOUND ... */
-			
-		$user = $app->user->firstOrNew(['student_id' => $student->id]);
-		$user->student_id = $student->id;
-		$user->school_id = $school_id;
-		$user->first_name = $first_name;
-		$user->last_name = $last_name;
-		$user->is_student = true;
-		$user->active = true;
-		$user->save();
 
-		$_SESSION[$app->config->get('auth.session')] = $user->id;
-		echo 'about call redirect';
-		$app->flash('global','You are now signed in');
-		$app->redirect($app->urlFor('home'));
-		die();	
+		if ($student) {
+			/* Set up a user record if there is not one already */
+			$user = $app->user->firstOrNew(['student_id' => $student->id]);
+			$user->student_id = $student->id;
+			$user->school_id = $school_id;
+			$user->first_name = $first_name;
+			$user->last_name = $last_name;
+			$user->is_student = true;
+			$user->active = true;
+			$user->save();
+
+			$_SESSION[$app->config->get('auth.session')] = $user->id;
+			echo 'about call redirect';
+			$app->flash('global','You are now signed in');
+			$app->redirect($app->urlFor('home'));
+		};	
 	}
 	
 	$app->render('auth/stulogin.php', [
