f5a7ff5182d41b5e0220f3e6ad5fe4e9fd481710
Tony Debling <tonydebling@gmail.com>
2017-09-17 07:52:43 +0100

    Revert "Enable debug in Slim"
    This reverts commit 98cdd62d28df0be833f1d6fb305a98688f889c49.

--- start.php
+++ start.php
@@ -33,7 +33,6 @@ require INC_ROOT . '/vendor/autoload.php';
 $app = new Slim([
 	'mode' => trim(file_get_contents(INC_ROOT . '/app/mode.php')),
 	'view' => new Twig(),
-	'debug' => true,
 	'templates.path' => INC_ROOT . '/app/views'
 ]);
 
